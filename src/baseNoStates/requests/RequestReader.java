22b186cc0315d8dbc3ae4f8b33793859cf57e66d
Yi Chen Xu <149014304+NIU1708964@users.noreply.github.com>
2025-10-19 01:23:14 +0200

    1.5
    Initial implementation of user groups

--- src/baseNoStates/requests/RequestReader.java
+++ src/baseNoStates/requests/RequestReader.java
@@ -4,7 +4,11 @@ import baseNoStates.DirectoryDoors;
 import baseNoStates.DirectoryUsers;
 import baseNoStates.Door;
 import baseNoStates.User;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
@@ -97,7 +101,96 @@ public class RequestReader implements Request {
     } else {
       //TODO: get the who, where, when and what in order to decide, and if not
       // authorized add the reason(s)
-      authorized = true;
+      String role = user.getRole();
+      LocalDateTime when = now;
+      switch (role) {
+          case "Admin":
+              authorized = true;
+              break;
+          case "Manager":
+              boolean valid = true;
+              // Date range inclusive: [2025-09-01, 2026-03-01]
+              LocalDate startDate = LocalDate.of(2025, 9, 1);
+              LocalDate endDate   = LocalDate.of(2026, 3, 1);
+
+              LocalDate d = when.toLocalDate();
+
+              if (d.isBefore(startDate) || d.isAfter(endDate)) {
+                  valid = false;
+                  addReason("Present date not in Manager schedule [2025-09-01..2026-03-01]");
+              }
+              // Day of week: Monday..Saturday
+
+              DayOfWeek dow = when.getDayOfWeek();
+              if (dow == DayOfWeek.SUNDAY) {
+                  valid = false;
+                  addReason("Present day of week not in Manager schedule [Mon–Sat]");
+              }
+
+              // Time window: [08:00, 20:00)
+              LocalTime start = LocalTime.of(8, 0);
+              LocalTime end   = LocalTime.of(20, 0);
+
+              LocalTime t = when.toLocalTime();
+              if (valid && (t.isBefore(start) || !t.isBefore(end))) {
+                  valid = false;
+                  addReason("Present time not in Manager schedule [08:00..20:00]");
+              }
+              // Actions: all are allowed for Manager
+              // Spaces: all spaces allowed for Manager; no need to check partition/space
+
+              authorized = valid;
+              break;
+          case "Employee":
+              boolean validE = true;
+              // Date range inclusive: [2025-09-01, 2026-03-01]
+              LocalDate startDateE = LocalDate.of(2025, 9, 1);
+              LocalDate endDateE   = LocalDate.of(2026, 3, 1);
+
+              LocalDate dE = when.toLocalDate();
+
+              if (dE.isBefore(startDateE) || dE.isAfter(endDateE)) {
+                  validE = false;
+                  addReason("Present date not in Employee schedule [2025-09-01..2026-03-01]");
+              }
+              // Day of week: Monday..Saturday
+
+              DayOfWeek dowE = when.getDayOfWeek();
+              if (dowE == DayOfWeek.SUNDAY || dowE == DayOfWeek.SATURDAY) {
+                  validE = false;
+                  addReason("Present day of week not in Employee schedule [Mon–Sat]");
+              }
+
+              // Time window: [08:00, 20:00)
+              LocalTime startE = LocalTime.of(9, 0);
+              LocalTime endE   = LocalTime.of(17, 0);
+
+              LocalTime tE = when.toLocalTime();
+              if (validE && (tE.isBefore(startE) || !tE.isBefore(endE))) {
+                  validE = false;
+                  addReason("Present time not in Employee schedule [09:00..17:00]");
+              }
+              if(action.equals("lock") || action.equals("unlock")) { // employee can't lock or unlock
+                  validE = false;
+                  addReason("Employees can't lock or unlock doors");
+              }
+
+              if(doorId.equals("D1") || doorId.equals("D2")) { // employees can't access parking doors
+                  validE = false;
+                  addReason("Employees can't access parking doors");
+              }
+              authorized = validE;
+              break;
+          case "": // can't do any actions
+              authorized = false;
+              break;
+          default:
+              authorized = false;
+              System.out.print("This user is " + user.getRole());
+              break;
+      }
+
+//      authorized = true; // true for debugging
     }
   }
 }
